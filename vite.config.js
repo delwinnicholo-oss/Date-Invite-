5dbfddf862d02dbc828375ac8cd4f7b0f4860efd
delwinnicholo-oss <delwinnicholo@gmail.com>
2026-06-24 22:34:45 +0530

    Updated file 3

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
   },
 })
